ef06262bc19c4109c5397f30997695be1f5d1555
nongiach <nongiach@gmail.com>
2020-01-11 10:06:23 +0100

    v8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version.startswith("2"):
     sys.exit(1)
 
 setup(name='QuicKeepass',
-        version='0.7',
+        version='0.8',
         author='@chaignc',
         url='https://github.com/nongiach/QuicKeepass',
         packages=['quickeepass'],
